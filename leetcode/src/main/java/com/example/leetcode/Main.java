2f7421f477a0f4d1a347c01552ee98245b30f241
OlehNoskov <olega6000@gmail.com>
2022-12-01 16:23:56 +0200

    Create project LeetCode

---
+++ leetcode/src/main/java/com/example/leetcode/Main.java
@@ -0,0 +1,10 @@
+package com.example.leetcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Oleg");
+        System.out.println("Oleg");
+        System.out.println("Oleg");
+    }
+}
